c70219835a635136a3522daf35f29d45a2ab5ed2
Joseph Daramola <daramolaj@gmail.com>
2020-04-13 03:31:26 +0100

    fixed responsiveness

--- src/components/StyledHero.js
+++ src/components/StyledHero.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import defaultImg from '../img/showcase.jpg';
 
 const StyledHero = styled.header`
-	min-height: ${(props) => (props.minHeight ? props.minHeight : '60vh')};
+	min-height: ${(props) => (props.minHeight ? props.minHeight : '75vh')};
 	background-color: rgba(0, 0, 0, 0.1);
 	background: url(${(props) => (props.img ? props.img : defaultImg)})
 		center/cover no-repeat;
